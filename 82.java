dfd3395dd31426868284d69de5a5e7fe978856ab
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-10 20:46:26 +0530

    area of a farm upto 5 decimal places

---
+++ 82.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		float l=s.nextFloat();
+		float b=s.nextFloat();
+		float area=l*b;
+		System.out.print(area);
+		
+	}
+}
